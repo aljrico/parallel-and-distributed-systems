809bdaccfddbc91640402a02254e0304b8b12ec7
Alfredo Hernández <aldomann.designs@gmail.com>
2018-02-19 23:59:24 +0100

    Fixed syntax

--- delivery4/word_count.scala
+++ delivery4/word_count.scala
@@ -1,5 +1,5 @@
 val f = sc.textFile("/hduser/input/pg2000.txt")
-val wc = f.flatMap(l => l.split(" "))
-          .map(word => (word, 1))
-          .reduceByKey(_ + _)
+val wc = f.flatMap(l => l.split(" ")).
+           map(word => (word, 1)).
+           reduceByKey(_ + _)
 wc.saveAsTextFile("/hduser/spark/wc_scala_out")
